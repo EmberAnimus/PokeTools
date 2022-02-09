0fd07d7e661cdb02b30ff90f07591cda0afad20b
Ember Animus <sebastian.rolett@gmail.com>
2022-02-09 02:02:28 -0600

    Update PokeTools.py

--- PokeTools.py
+++ PokeTools.py
@@ -25,9 +25,9 @@ class uiInfo:
     def getMoves(pokemon, game):
         #Get TM/HM, Tutor, Egg, and LvlUp learnsets.
         tutorMoves = []
-        cur.execute("""SELECT wp.name, wm.name, wm3.type_1, wm3.base_power, wm3.power_points, wm3.accuracy, wm3.priority,
+        tutorSqlQuery = """SELECT wp.name, wm.name, wm3.type_1, wm3.base_power, wm3.power_points, wm3.accuracy, wm3.priority,
                         wm3.damage_category, wm3.effect, wm3.description, wg1.name
-                        FROM westwood_pokemon wp 
+                        FROM {} wp 
                         INNER JOIN westwood_pokemontutorsets wp1 ON ( wp.name = wp1.name  )  
                         INNER JOIN westwood_tutorsetslistelement wt ON ( wp1.tutor_sets = wt.list_id  )  
                         INNER JOIN westwood_tutorset wt1 ON ( wt.element_id = wt1.id  )  
@@ -39,28 +39,21 @@ class uiInfo:
                         INNER JOIN westwood_movedefinition wm3 ON ( wm2.move_definition_id = wm3.id  )  
                         INNER JOIN westwood_gameslistelement wg ON ( wm2.games = wg.list_id  )  
                         INNER JOIN westwood_game wg1 ON ( wg.element_id = wg1.id  ) 
-                        WHERE wp.name = ? AND wg1.name IN (?) """, (pokemon,game))
-                        #Selects move data using westwood_pokemon. The reason its a long call is that most of the data is linked via index tables - this is
-                        #due to the data complexity.
+                        WHERE wp.name = ? AND wg1.name IN (?) """
+        
+        tmSqlQuery = """"""
+
+        learnsetSqlQuery = """"""
+        
+        #Base query for the the various move data. The only changes we have to make are calling the pokemon and pokemonform tables.
+        #Being they aren't fk joined, we can't really join each respective queries into a single execute function.
+
+        sqlPokemonTable = "westwood_pokemon"
+        sqlPokemonFormsTable = "westwood_pokemonform"
+        cur.execute(tutorSqlQuery.format(sqlPokemonTable), (pokemon,game))
         tutorMovesTemp = cur.fetchall()
         tutorMoves.append(tutorMovesTemp)
-        cur.execute("""SELECT wp.name, wm.name, wm3.type_1, wm3.base_power, wm3.power_points, wm3.accuracy, 
-                        wm3.priority, wm3.damage_category, wm3.effect, wm3.description, wg1.name
-                        FROM westwood_pokemonform wp 
-                        INNER JOIN westwood_pokemontutorsets wp1 ON ( wp.name = wp1.name  )  
-                        INNER JOIN westwood_tutorsetslistelement wt ON ( wp1.tutor_sets = wt.list_id  )  
-                        INNER JOIN westwood_tutorset wt1 ON ( wt.element_id = wt1.id  )  
-                        INNER JOIN westwood_tutorsetmoveslistelement wt2 ON ( wt1.tutor_set_moves = wt2.list_id  )  
-                        INNER JOIN westwood_tutorsetmove wt3 ON ( wt2.element_id = wt3.id  )  
-                        INNER JOIN westwood_move wm ON ( wt3.name = wm.name  )  
-                        INNER JOIN westwood_moverecordslistelement wm1 ON ( wm.move_records = wm1.list_id  )  
-                        INNER JOIN westwood_moverecord wm2 ON ( wm1.element_id = wm2.id  )  
-                        INNER JOIN westwood_movedefinition wm3 ON ( wm2.move_definition_id = wm3.id  )  
-                        INNER JOIN westwood_gameslistelement wg ON ( wm2.games = wg.list_id  )  
-                        INNER JOIN westwood_game wg1 ON ( wg.element_id = wg1.id  ) 
-                        WHERE wp.name = ? AND wg1.name IN (?) """, (pokemon,game))
-                        #Selects move data using westwood_pokemonforms. The reason its a long call is that most of the data is linked via index tables - this is
-                        #due to the data complexity. This appends to the existing list.
+        cur.execute(tutorSqlQuery.format(sqlPokemonFormsTable), (pokemon,game))
         tutorMovesTemp.append(cur.fetchall())
         tutorMoves.append(cur.fetchall())
         return tutorMoves     
